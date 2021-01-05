c40c1b5960f5a088cb8a6f5a0bc5afe9fd4edd6f
CofChips <qiwei.mod@gmail.com>
2021-01-05 15:33:08 -0800

    added functionality for movie search

--- liri.js
+++ liri.js
@@ -6,10 +6,12 @@ const axios = require("axios")
 const spotify = keys.spotify
 
 let artist = ""
+let artistFormat = ""
 
 if (process.argv[2] === "concert-this"){
     for(let i = 3; i < process.argv.length; i++){
         artist += process.argv[i]
+        artistFormat += process.argv[i] + " "
     }
 
     let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
@@ -25,16 +27,44 @@ if (process.argv[2] === "concert-this"){
         if(names.length === 0){
             console.log("No results found")
         }
-        else{console.log("Results for: ",artist,"\n", names)}
+        else{console.log("Results for: ",artistFormat,"\n", names)}
     })
-} else{
+} 
+else if(process.argv[2] === "movie-this"){
+    let movie=""
+
+    for(let i = 3; i < process.argv.length; i++){
+        movie += process.argv[i] + "%20"
+    }
+
+    let queryURL ="https://www.omdbapi.com/?t="+movie+"&apikey=trilogy"
+
+    axios.get(queryURL).then(function(res){
+        let rotten = ""
+        if(res.data.Title != null){
+            for (let i = 0; i < res.data.Ratings.length; i++){
+                if(res.data.Ratings[i].Source === "Rotten Tomatoes"){
+                    rotten = res.data.Ratings[i].Value
+                }
+            }
+        }
+        console.log(`
+        Title: ${res.data.Title}
+        Year: ${res.data.Year}
+        IMDB Rating: ${res.data.imdbRating}
+        Rotten Tomatoes Rating: ${rotten}
+        Country: ${res.data.Country}
+        Language: ${res.data.Lanuage}
+        Plot: ${res.data.Plot}
+        Actors: ${res.data.Actors}
+        `)
+    })
+}
+else{
 
     console.log("something")
 }
 
 
-
-
-//console.log(spotify)
 
 
